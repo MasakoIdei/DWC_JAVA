4bc511c8e126feeee88c830eb0690158df742fbe
MasakoIdei <tarte.groseilles@gmail.com>
2021-09-12 23:43:14 +0900

    確認問題02追加

--- Chapter02.java
+++ Chapter02.java
@@ -1,7 +1,18 @@
-//確認問題
-//ファイルを作成し、「こんにちは！」と表示してください。
+/*確認問題01
+ファイルを作成し、「こんにちは！」と表示してください。
 public class Chapter02 {
     public static void main(String[] args) {
+
         System.out.println("こんにちは！");
+    }
+}
+*/
+
+//確認問題02
+//今度は、Hello Webcamp と Hello Java!の２文字を出力してください。
+ public class Chapter02 {
+    public static void main(String[] args) {
+        System.out.println("Hello Webcamp!");
+        System.out.println("Hello Java!");
     }
 }
