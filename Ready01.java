3c509963cae62ae3c846368dda6953aa94cf5f0c
MasakoIdei <tarte.groseilles@gmail.com>
2021-09-12 23:36:28 +0900

    JAVAの構成について

--- Ready01.java
+++ Ready01.java
@@ -1,5 +1,17 @@
+/*
+・JAVAのソースコードは、ブロックと文で構成されている。
+　ブロック ＝｛｝波かっこで囲まれたプログラムの塊　※railsだとdo～endになってる部分
+　　　文 　＝ ;セミコロンで終わる
+*/
+
+
+//クラスブロック
 public class Ready01 {
+
+    //  メゾットブロック
     public static void main(String[] args) {
+
+        //メゾットブロックの処理
         System.out.println("Hello World!");
     }
 }
