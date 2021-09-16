92a760bffc9bbd05fd1975376ee34cb48f02e9fe
MasakoIdei <tarte.groseilles@gmail.com>
2021-09-16 23:34:31 +0900

    ５章　演習問題

---
+++ Chapter05.java
@@ -0,0 +1,10 @@
+//演習問題
+//以下のプログラムを作成してください。
+//プリミティブ型の「long」「double」「boolean」の変数に値を代入して出力
+//　・ラッパークラスの「Float」「Integer」「Character」の変数に値を代入して出
+//・要素数が5の「int」配列に値を代入して、インデックス番号が3の値を出力
+public class Chapter05 {
+    public static void main(String[] args) {
+
+    }
+}
